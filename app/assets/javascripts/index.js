052d9d8decb735c456944e16eabb50d3bc541cd1
JohannOnGitHub <johann.fromont@gmail.com>
2017-07-28 10:31:17 +0200

    carousel

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,32 @@
-/* var main = function() {
-  
+/* var main = function () {
+        'use strict';
+  $('.arrow-next').click(function () {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
+    if (nextSlide.length === 0) {
+       nextSlide = $('.slide').first();
+       nextDot = $('.dot').first();
+    }
+
+  $('.arrow-prev').click(function () {
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
+
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+}
+
+$(document).ready(main); */
+      
+var main = function() {
+
   $('.arrow-next').click(function() {
     var currentSlide = $('.active-slide');
     var nextSlide = currentSlide.next();
@@ -11,6 +38,14 @@
       nextSlide = $('.slide').first();
       nextDot = $('.dot').first();
     }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+  });
+
 
   $('.arrow-prev').click(function() {
     var currentSlide = $('.active-slide');
@@ -23,7 +58,14 @@
       prevSlide = $('.slide').last();
       prevDot = $('.dot').last();
     }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+  });
+
 }
 
-$(document).ready(main); */
-      
+$(document).ready(main);
